8a658fb1f13768a197e05c75793455daa726cb1b
1223Daniil <mashtallerdaniil@gmail.com>
2024-02-20 17:22:50 +0300

    little change 2

--- components/MainPage/SelectionClothes.vue
+++ components/MainPage/SelectionClothes.vue
@@ -24,7 +24,7 @@
           <img
             width="50"
             height="40"
-            src="../../publicimage/components/SelectionClothes/image 100 (Traced).png"
+            src="../../public/image/components/SelectionClothes/image 100 (Traced).png"
           />
         </template>
         <template v-slot:subtitle>
@@ -59,19 +59,19 @@ export default {
       optionstwo: ["Спорт", "Одежда"],
       optionstree: ["Мбаппе", "Месси", "Роналду"],
       slideone: [
-        { image: "/image/components/SelectionClothes/Rectangle 7042.png" },
-        { image: "/image/components/SelectionClothes/Rectangle 7042 (2).png" },
-        { image: "/image/components/SelectionClothes/Rectangle 7042 (1).png" },
+        { image: "../../public/image/components/SelectionClothes/Rectangle 7042.png" },
+        { image: "../../public/image/components/SelectionClothes/Rectangle 7042 (2).png" },
+        { image: "../../public/image/components/SelectionClothes/Rectangle 7042 (1).png" },
       ],
       slidetwo: [
-        { image: "/image/components/SelectionClothes/Rectangle 7042 (1).png" },
-        { image: "/image/components/SelectionClothes/Rectangle 7042.png" },
-        { image: "/image/components/SelectionClothes/Rectangle 7042 (2).png" },
+        { image: "../../public/image/components/SelectionClothes/Rectangle 7042 (1).png" },
+        { image: "../../public/image/components/SelectionClothes/Rectangle 7042.png" },
+        { image: "../../public/image/components/SelectionClothes/Rectangle 7042 (2).png" },
       ],
       slidetree: [
-        { image: "/image/components/SelectionClothes/Rectangle 7042 (2).png" },
-        { image: "/image/components/SelectionClothes/Rectangle 7042 (1).png" },
-        { image: "/image/components/SelectionClothes/Rectangle 7042.png" },
+        { image: "../../public/image/components/SelectionClothes/Rectangle 7042 (2).png" },
+        { image: "../../public/image/components/SelectionClothes/Rectangle 7042 (1).png" },
+        { image: "../../public/image/components/SelectionClothes/Rectangle 7042.png" },
       ],
     };
   },
